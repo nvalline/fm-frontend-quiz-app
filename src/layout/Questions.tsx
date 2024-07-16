ca584c0d421ca659a8d38fda1e9bfb8a9b38bdc3
vall4251n <58278138+nvalline@users.noreply.github.com>
2024-07-16 15:48:01 -0600

    added status icon script for correct/incorrect answer

--- src/layout/Questions.tsx
+++ src/layout/Questions.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useDataContext } from '../contexts/dataContext';
 import correctIcon from '../assets/images/icon-correct.svg';
 import errorIcon from '../assets/images/icon-error.svg';
@@ -12,12 +12,24 @@ type CategoryProps = {
 	category: string;
 };
 
+type AnswerState = {
+	correctAnswer: string;
+	correctAnswerIndex: number | null;
+	isCorrect: boolean | null;
+};
+
 export default function Questions({ category }: CategoryProps) {
 	const { data } = useDataContext();
-	const [correctAnswer, setCorrectAnswer] = useState<string>('');
-	const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
-	const [score, setScore] = useState<number>(0);
+	const [choice, setChoice] = useState<string>('');
 	const [questionNumber, setQuestionNumber] = useState<number>(0);
+	const [score, setScore] = useState<number>(0);
+	const [selected, setSelected] = useState<number | null>(null);
+
+	const [answerState, setAnswerState] = useState<AnswerState>({
+		correctAnswer: '',
+		correctAnswerIndex: null,
+		isCorrect: null
+	});
 
 	const quizData = JSON.parse(JSON.stringify(data.quizzes));
 
@@ -40,31 +52,53 @@ export default function Questions({ category }: CategoryProps) {
 
 	const questionData = quiz.questions[questionNumber];
 
-	const handleChoice = (event: React.MouseEvent<HTMLButtonElement>) => {
+	// console.log('QUESTION DATA', questionData);
+
+	const handleChoice = (
+		event: React.MouseEvent<HTMLButtonElement>,
+		index: number,
+		option: string
+	) => {
+		const answer = questionData.answer;
+
 		event.preventDefault();
 
-		console.log('EVENT:', event.currentTarget.value);
+		setSelected(index);
+		setChoice(option);
+		setAnswerState((prevState) => {
+			prevState.correctAnswer = questionData.answer;
+			prevState.correctAnswerIndex = questionData.options.indexOf(answer);
+			return { ...prevState };
+		});
 
-		if (event.currentTarget.value === correctAnswer) {
-			console.log('CORRECT ANSWER');
-			setIsCorrect(true);
+		// console.log('INDEX:', index);
+		// console.log('EVENT:', option);
+		// console.log('ANSWER STATE:', answerState);
+	};
+
+	const handleSubmit = (event: React.MouseEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		if (choice === answerState.correctAnswer) {
+			// console.log('CORRECT ANSWER');
+			setAnswerState((prevState) => {
+				prevState.isCorrect = true;
+				return { ...prevState };
+			});
 			setScore(score + 1);
 		} else {
-			console.log('WRONG ANSWER');
-			setIsCorrect(false);
+			// console.log('WRONG ANSWER');
+			setAnswerState((prevState) => {
+				prevState.isCorrect = false;
+				return { ...prevState };
+			});
 		}
 	};
 
-	console.log('SCORE:', score);
-
-	useEffect(() => {
-		setCorrectAnswer(questionData.answer);
-	}, [questionData, setCorrectAnswer]);
-
-	// console.log('Question:', questionData);
+	// console.log('SCORE:', score);
 
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className='questionWrapper'>
 				<p>{`Question ${questionNumber + 1} of ${quiz.questions.length}`}</p>
 				<h2>{questionData.question}</h2>
@@ -75,26 +109,40 @@ export default function Questions({ category }: CategoryProps) {
 				></progress>
 			</div>
 			<ul className='optionsWrapper'>
-				{questionData.options.map((option: string, index: number) => (
-					<li key={index}>
-						<button type='button' value={option} onClick={handleChoice}>
-							<span className='optionIdentifier'>
-								{String.fromCharCode(65 + index)}
-							</span>
-							{option}
-							<span>
-								{isCorrect !== null &&
-									(isCorrect ? (
-										<img src={correctIcon} alt='correct' />
-									) : (
-										<img src={errorIcon} alt='incorrect' />
-									))}
-							</span>
-						</button>
-					</li>
-				))}
+				{questionData.options.map((option: string, index: number) => {
+					const showCheckMark =
+						(answerState.isCorrect === true &&
+							answerState.correctAnswerIndex === index) ||
+						(answerState.isCorrect === false &&
+							answerState.correctAnswerIndex === index);
+					const showXMark =
+						answerState.isCorrect === false && selected === index;
+
+					return (
+						<li key={index}>
+							<button
+								value={option}
+								onClick={(event) => handleChoice(event, index, option)}
+								className={`optionButton ${
+									selected === index ? 'selected' : ''
+								}`}
+							>
+								<span className='optionIdentifier'>
+									{String.fromCharCode(65 + index)}
+								</span>
+								{option}
+								<span className='statusIcon'>
+									{showCheckMark && <img src={correctIcon} alt='correct' />}
+									{showXMark && <img src={errorIcon} alt='incorrect' />}
+								</span>
+							</button>
+						</li>
+					);
+				})}
 			</ul>
-			<button className='submitButton'>Submit Answer</button>
+			<button type='submit' className='submitButton'>
+				Submit Answer
+			</button>
 		</form>
 	);
 }
